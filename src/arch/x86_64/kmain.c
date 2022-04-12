c182cc5ed9d3b9b62a32b132b3395c844d9535d7
kcjennin <kcjennin@calpoly.edu>
2022-04-11 23:51:04 -0700

    shorts done in printk

--- src/arch/x86_64/kmain.c
+++ src/arch/x86_64/kmain.c
@@ -8,7 +8,22 @@ void kmain(void)
     // while(!j);
 
     VGA_clear();
-    printk("Hi! %c\n", 48);
+    // normals
+    // printk("string: %s\n", "Hello, World!");
+    // printk("d: %d\n", 0);
+    // printk("u: %u\n", 0);
+    // printk("x: 0x%x\n", 0);
+    // printk("c: %c\n", '\0');
+
+    // longs
+    // printk("ld: %ld\n", -10000000L);
+    // printk("lu: %lu\n", 10000000L);
+    // printk("lx: 0x%lx\n", -1L);
+
+    // shorts
+    // printk("hd: %hd\n", -12000);
+    // printk("hu: %hu\n", 12000);
+    // printk("hx: 0x%hx\n", -1);
 
     while (1) asm("hlt");
 }
--- src/arch/x86_64/printk.c
+++ src/arch/x86_64/printk.c
@@ -1,20 +1,26 @@
 #include <stdarg.h>
 #include <vga.h>
 
-#define MAX_INT_STR 11
+#define MAX_INT_STR 10
+#define MAX_LONG_INT_STR 20
+#define MAX_SHORT_INT_STR 5
 #define MAX_HEX_STR 8
+#define MAX_LONG_HEX_STR 16
+#define MAX_SHORT_HEX_STR 4
 
 // %% %d %u %x %c %p %h[dux] %l[dux] %q[dux] %s
-void print_decimal(int i)
+void print_int(int i)
 {
     int negative = 0, index = MAX_INT_STR - 2;
-    char str[MAX_INT_STR] = { 0 };
+    char str[MAX_INT_STR + 1] = { 0 };
 
     if (i < 0)
     {
         negative = 1;
         i *= -1;
     }
+    else if (!i)
+        str[index--] = '0';
 
     for (; i; i /= 10)
         str[index--] = (i % 10) + '0';
@@ -24,10 +30,83 @@ void print_decimal(int i)
     VGA_display_str(&str[index+1]);
 }
 
-void print_unsigned_decimal(unsigned int i)
+void print_long_int(long int i)
+{
+    int negative = 0, index = MAX_LONG_INT_STR - 2;
+    char str[MAX_LONG_INT_STR + 1] = { 0 };
+
+    if (i < 0)
+    {
+        negative = 1;
+        i *= -1;
+    }
+    else if (!i)
+        str[index--] = '0';
+
+    for (; i; i /= 10)
+        str[index--] = (i % 10) + '0';
+
+    if (negative) str[index--] = '-';
+
+    VGA_display_str(&str[index+1]);
+}
+
+void print_short_int(short int i)
+{
+    int negative = 0, index = MAX_SHORT_INT_STR - 2;
+    char str[MAX_SHORT_INT_STR + 1] = { 0 };
+
+    if (i < 0)
+    {
+        negative = 1;
+        i *= -1;
+    }
+    else if (!i)
+        str[index--] = '0';
+
+    for (; i; i /= 10)
+        str[index--] = (i % 10) + '0';
+
+    if (negative) str[index--] = '-';
+
+    VGA_display_str(&str[index+1]);
+}
+
+void print_unsigned_int(unsigned int i)
 {
     int index = MAX_INT_STR - 2;
     char str[MAX_INT_STR] = { 0 };
+
+    if (!i)
+        str[index--] = '0';
+
+    for (; i; i /= 10)
+        str[index--] = (i % 10) + '0';
+
+    VGA_display_str(&str[index+1]);
+}
+
+void print_long_unsigned_int(long unsigned int i)
+{
+    int index = MAX_LONG_INT_STR - 2;
+    char str[MAX_LONG_INT_STR] = { 0 };
+
+    if (!i)
+        str[index--] = '0';
+
+    for (; i; i /= 10)
+        str[index--] = (i % 10) + '0';
+
+    VGA_display_str(&str[index+1]);
+}
+
+void print_short_unsigned_int(short unsigned int i)
+{
+    int index = MAX_SHORT_INT_STR - 2;
+    char str[MAX_SHORT_INT_STR] = { 0 };
+
+    if (!i)
+        str[index--] = '0';
 
     for (; i; i /= 10)
         str[index--] = (i % 10) + '0';
@@ -38,8 +117,9 @@ void print_unsigned_decimal(unsigned int i)
 void print_unsigned_hex(unsigned int i)
 {
     const char hex_lookup[] = "0123456789abcdef";
-    char str[MAX_HEX_STR + 2] = { 0 };
-    int n, len = 0;
+    char str[MAX_HEX_STR] = { 0 };
+    unsigned int n;
+    int len = 0;
 
     if (!i)
         len = 1;
@@ -51,8 +131,54 @@ void print_unsigned_hex(unsigned int i)
 
     str[len] = '\0';
 
-    for (--len, n = i; len >= 0; n >>=4, --len)
-        str[len] = hex_lookup[n & 0xf];
+    for (--len; len >= 0; i>>=4, --len)
+        str[len] = hex_lookup[i & 0xf];
+
+    VGA_display_str(str);
+}
+
+void print_long_unsigned_hex(long unsigned int i)
+{
+    const char hex_lookup[] = "0123456789abcdef";
+    char str[MAX_LONG_HEX_STR] = { 0 };
+    long unsigned int n;
+    int len = 0;
+
+    if (!i)
+        len = 1;
+    else
+    {
+        for (n = i; n; n >>= 4)
+            len++;
+    }
+
+    str[len] = '\0';
+
+    for (--len; len >= 0; i>>=4, --len)
+        str[len] = hex_lookup[i & 0xf];
+
+    VGA_display_str(str);
+}
+
+void print_short_unsigned_hex(short unsigned int i)
+{
+    const char hex_lookup[] = "0123456789abcdef";
+    char str[MAX_SHORT_HEX_STR] = { 0 };
+    short unsigned int n;
+    int len = 0;
+
+    if (!i)
+        len = 1;
+    else
+    {
+        for (n = i; n; n >>= 4)
+            len++;
+    }
+
+    str[len] = '\0';
+
+    for (--len; len >= 0; i>>=4, --len)
+        str[len] = hex_lookup[i & 0xf];
 
     VGA_display_str(str);
 }
@@ -69,7 +195,7 @@ void print_pointer(void *ptr)
 
 void print_str(const char *str)
 {
-
+    VGA_display_str(str);
 }
 
 void print_uchar(unsigned char c)
@@ -82,22 +208,11 @@ void print_short(short s)
 
 }
 
-void print_long_hex(long l)
-{
-
-}
-
 void printk(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
 
-    int d;
-    unsigned int ux;
-    char c;
-    // h?
-    // l?
-    // q is ll
     char * str;
 
     while (*fmt != '\0')
@@ -107,38 +222,66 @@ void printk(const char *fmt, ...)
             fmt++;
             switch (*fmt)
             {
-                case '%':
-                    VGA_display_char('%');
-                    break;
+            case '%':
+                VGA_display_char('%');
+                break;
+            case 'd':
+                print_int(va_arg(args, int));
+                break;
+            case 'u':
+                print_unsigned_int(va_arg(args, unsigned int));
+                break;
+            case 'x':
+                print_unsigned_hex(va_arg(args, unsigned int));
+                break;
+            case 'c':
+                print_char(va_arg(args, int));
+                break;
+            case 'p':
+                print_pointer(va_arg(args, void *));
+                break;
+            case 'h':
+                fmt++;
+                switch (*fmt)
+                {
                 case 'd':
-                    print_decimal(va_arg(args, int));
+                    print_short_int(va_arg(args, int));
                     break;
                 case 'u':
-                    print_unsigned_decimal(va_arg(args, unsigned int));
+                    print_short_unsigned_int(va_arg(args, unsigned int));
                     break;
                 case 'x':
-                    print_unsigned_hex(va_arg(args, unsigned int));
-                    break;
-                case 'c':
-                    print_char(va_arg(args, int));
-                    break;
-                case 'p':
-                    print_pointer(va_arg(args, void *));
-                    break;
-                case 'h':
-                    // TODO
-                    break;
-                case 'l':
-                    // TODO
-                    break;
-                case 'q':
-                    // TODO
-                    break;
-                case 's':
-                    print_str(va_arg(args, char *));
+                    print_short_unsigned_hex(va_arg(args, unsigned int));
                     break;
                 default:
                     break;
+                }
+                break;
+            case 'l':
+                fmt++;
+                switch (*fmt)
+                {
+                case 'd':
+                    print_long_int(va_arg(args, long int));
+                    break;
+                case 'u':
+                    print_long_unsigned_int(va_arg(args, long unsigned int));
+                    break;
+                case 'x':
+                    print_long_unsigned_hex(va_arg(args, long unsigned int));
+                    break;
+                default:
+                    break;
+                }
+                break;
+            case 'q':
+                // multiple options
+                break;
+            case 's':
+                print_str(va_arg(args, char *));
+                break;
+            default:
+                break;
             }
         }
         else
@@ -146,5 +289,4 @@ void printk(const char *fmt, ...)
         fmt++;
     }
     va_end(args);
-    VGA_display_char('\n');
 }
